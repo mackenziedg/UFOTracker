053928ce9c530198e007b0806284195285e216f5
Mackenzie Gray <mackenzie.gray13@ncf.edu>
2017-11-28 11:50:00 -0500

    Adds plotting on a map.

--- src/ufo_cleaning/geocode_locs.py
+++ src/ufo_cleaning/geocode_locs.py
@@ -1,7 +1,46 @@
+from itertools import chain
+from multiprocessing import Pool
+import numpy as np
 import pandas as pd
 import re
 from geopy.geocoders import GoogleV3
 from time import sleep
+
+
+def code_names(n):
+    """Geocodes an array of placenames to longitudes and latitudes using Google Maps API.
+
+    Parameters
+    ----------
+    n -- An array of place names as strings
+
+    Returns
+    -------
+    lons -- An array of longitudes for each input place (None if that place failed to geocode properly)
+    lats -- An array of latitudes for each input place (None if that place failed to geocode properly)
+    """
+    g = GoogleV3(api_key="AIzaSyC8AudJMjThy44TnVdsM195imL4XboSpng")
+
+    lats, lons = [], []
+    failed = []
+    for ix, place in enumerate(n):
+        try:
+            loc = g.geocode(place, timeout=10)
+            lats.append(loc.latitude)
+            lons.append(loc.longitude)
+        except:
+            lats.append(None)
+            lons.append(None)
+            print('x', end='', flush=True)
+            continue
+
+        if ix % 100 == 0:
+            print(ix, end='', flush=True)
+        elif ix % 10 == 0:
+            print('.', end='', flush=True)
+
+    return lons, lats
+
 
 # Read in only data from and after 1974
 df = pd.read_csv("../../data/raw/ufo_sightings.csv")
@@ -15,26 +54,24 @@ unique_locs = list(unique_locs.unique())
 unique_locs = [i for i in unique_locs if type(i) is not float]
 unique_locs = [i for i in unique_locs if re.match(r"^[0-9A-z ,.]+$", i)]
 
-# Geocodes each unique location
-# Cuts down the number needed from >100k to ~20k
-# Doing per item is maybe slower but allows to see where the
-# script left off if it crashes
-g = GoogleV3(api_key="no show")
+# Apply the geocoding to each subset of the unique_locs
+n_workers = 30
+p = Pool(n_workers)
 
-lats, lons = [], []
-failed = []
-for ix, place in enumerate(unique_locs):
-    try:
-        loc = g.geocode(place, timeout=10)
-    except:
-        failed.append(ix)
-        continue
+ranges = list(range(0, len(unique_locs), int(len(unique_locs)/n_workers)))
+ranges[-1] = len(unique_locs)
+ranges = [(v, ranges[i+1]) for i, v in enumerate(ranges[:-1])]
 
-    lats.append(loc.latitude)
-    lons.append(loc.longitude)
-    if ix % 100 == 0:
-        print(ix)
-    else:
-        print('.', end='', flush=True)
-    # sleep(0.1)
+# Outputs a `n_workers`-length array of sets of (lat, lon) tuples
+out = p.map(code_names, [unique_locs[i[0]:i[1]] for i in ranges])
 
+# Restructure the lats and lons
+lats = [i[0] for i in out]
+lons = [i[1] for i in out]
+lats = list(chain.from_iterable(lats))
+lons = list(chain.from_iterable(lons))
+
+# Write to file
+df = pd.DataFrame({'loc': unique_locs, 'lat': lats, 'lon': lons})
+df = df.dropna()
+df.to_csv("../../data/raw/geocode.csv", index=None)
---
+++ src/ufo_cleaning/plot_base_locations.py
@@ -0,0 +1,33 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import us
+
+sns.set_style('whitegrid')
+
+geo = pd.read_csv("../../data/raw/geocode.csv")
+sightings = pd.read_csv("../../data/raw/ufo_sightings.csv")
+sightings['loc'] = sightings.city + ", " + sightings.state
+
+sightings = sightings.join(geo.set_index('loc'), on='loc')
+sightings = sightings.dropna(subset=['lat'])
+sightings = sightings[sightings.state.isin([st.abbr for st in us.states.STATES_CONTIGUOUS])]
+
+# Limit the sightings to valid lat/long pairs in the contiguous USA
+top = 49.3457868 # north lat
+left = -124.7844079 # west long
+right = -66.9513812 # east long
+bottom = 24.7433195 # south lat
+sightings = sightings[(sightings.lat.between(bottom, top))&(sightings.lon.between(left, right))]
+
+
+m = Basemap(projection='merc', llcrnrlat=bottom, urcrnrlat=top, llcrnrlon=left, urcrnrlon=right, lat_ts=None, resolution='l')
+m.drawcoastlines()
+m.drawcountries()
+m.drawstates()
+m.drawmapboundary(color='white')
+lon, lat = m(sightings.lon.values, sightings.lat.values)
+m.scatter(lon, lat, color='#174793', alpha=0.01)
+plt.show()
